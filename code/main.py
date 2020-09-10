bc46efce56125be59aad36c5919564d82bea896e
Michael <chelmto3000@gmail.com>
2020-09-10 19:45:09 -0300

    Implementaçao de funçoes para trabalhar com css e multiplas janelas

--- code/main.py
+++ code/main.py
@@ -3,24 +3,56 @@
 
 import gi
 import os
+os.system("rm -rf ~/crud-python/code/UtilGtk; ln -s ~/crud-python/util/compiled/ UtilGtk")
+import sqlite3
+import datetime
+from UtilGtk.mikeUtil import MikeGtk
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 
 ui = Gtk.Builder()
-ui.add_from_file("arquivo_de_interface")
+ui.add_from_file("interface.ui")
 
-class Handler(object):
+class Handler(MikeGtk):
 
-    def __init__(self, *args, **kwargs):
-        super(Handler, self).__init__(*args, **kwargs)
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    # -->  Variáveis globais
+	def __init__(self, *args, **kwargs):
+		super(Handler, self).__init__(*args, **kwargs)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# -->  Variáveis globais
+		# Janela de Login
+		self.jan_login = ui.get_object("jn_login")
+		self.jan_login.show_all()
+		self.entry_usuario = ui.get_object("entry_usuario")
+		self.entry_senha = ui.get_object("entry_senha")
+	
+		# Message Dialog
+		self.msg_box = ui.get_object("msg_box")
+		
+		# janela principal
+		self.jn_principal = ui.get_object("jn_principal")
+		
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	# --> Fechar janela de login
+	def ao_destruir_jn_login(self, *args):
+		pass
+		
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	# --> Executa o login
+	def ao_clicar_em_bt_login(self, *args):
+		pass
 
-ui.connect_signals(Handler())
-window = ui.get_object("sua_primeira_janela")
-window.show_all()
-
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	#--> Chamar uma nova Janela no mesmo arquivo glade
+	def new_window_main(self, new_window, now_window,  modal=False, 
+						destroy_parent=False, maximize=False):
+		new_window.set_transient_for(now_window)
+		new_window.set_modal(modal)
+		new_window.set_destroy_with_parent(destroy_parent)
+		new_window.show_all()
+		if maximize == True:
+			new_window.maximize()
+			
 if __name__ == '__main__':
+    ui.connect_signals(Handler())
     Gtk.main()
-
